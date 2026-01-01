28c59d82c0189ce124715a02d2b8f2a6d28abdc5
riabi1 <riabimedali18@gmail.com>
2026-01-01 12:40:59 +0100

    Exercise 2: Toggle Visibility

--- 2__State_and_Interaction/src/App.jsx
+++ 2__State_and_Interaction/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [count, setCount] = useState(0)
-  const increment = () => setCount(prev => prev + 1)
+  const [isVisible, setIsVisible] = useState(false)
+  const toggle = () => { setIsVisible(prev => !prev) }
 
   return (
     <div>
-      <p>count {count}</p>
-      <button onClick={increment}>Increment</button>
+      {isVisible && <div>Now you see me</div>}
+      <button onClick={toggle}>{isVisible ? 'Hide' : 'Show'}</button>
     </div>
   )
 }
