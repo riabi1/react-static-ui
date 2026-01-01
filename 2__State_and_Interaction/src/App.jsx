be68503e040c0f258df0527c569cc495e9ecad8b
riabi1 <riabimedali18@gmail.com>
2026-01-01 13:17:47 +0100

    Exercise 5: Controlled Input

--- 2__State_and_Interaction/src/App.jsx
+++ 2__State_and_Interaction/src/App.jsx
@@ -1,20 +1,13 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false)
-  const toggleModal = () => setIsModalOpen(prev => !prev)
+  const [inputValue, setInputValue] = useState('')
 
   return (
     <div>
-      <button onClick={toggleModal}>Open Modal</button>
-      {isModalOpen && (
-        <div className="overlay">
-          <div className="modal">
-            <p>This is a modal</p>
-            <button onClick={toggleModal}>Close</button>
-          </div>
-        </div>
-      )}
+      <input value={inputValue} onChange={e => setInputValue(e.target.value)} />
+      <p>{inputValue}</p>
+      <button onClick={() => setInputValue('')}>Clear</button>
     </div>
   )
 }
